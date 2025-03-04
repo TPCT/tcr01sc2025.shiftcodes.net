e1a06ae17eca6c1b2ba5850f004ad7e0e6eb69db
Hossaamhassan <hossam.h.eldeeb@gmail.com>
2025-03-04 22:09:52 +0200

    Upload

--- Modules/Website/resources/views/layouts/parts/faq.blade.php
+++ Modules/Website/resources/views/layouts/parts/faq.blade.php
@@ -11,27 +11,26 @@
                 </div>
 
                 <div class="col-lg-12">
-                    <div class="accordion" id="accordionExample">
-                        @foreach($faq as $f)
-                        <div class="card">
-                          <div class="card-header" id="headingOne{{$f->id}}">
-                            <h2 class="mb-0">
-                              <button class="btn btn-link btn-block text-left" type="button" data-toggle="collapse" data-target="#collapseOne{{$f->id}}" aria-expanded="true" aria-controls="collapseOne{{$f->id}}">
-                                {{$f->question}}
-                                <i class="fa fa-angle-down"></i>
-                              </button>
-                            </h2>
-                          </div>
-                      
-                          <div id="collapseOne{{$f->id}}" class="collapse" aria-labelledby="headingOne{{$f->id}}" data-parent="#accordionExample">
-                            <div class="card-body">
-                                {{$f->answer}}
-                                
-                            </div>
-                          </div>
-                        </div>
-                        @endforeach
-                    </div>
+                <div class="accordion" id="accordionExample">
+    @foreach($faq as $f)
+    <div class="card">
+        <div class="card-header" id="headingOne{{$f->id}}">
+            <h2 class="mb-0">
+                <button class="btn btn-link btn-block text-left" type="button" data-toggle="collapse" data-target="#collapseOne{{$f->id}}" aria-expanded="false" aria-controls="collapseOne{{$f->id}}">
+                    {{$f->question}}
+                    <i class="fa fa-plus icon"></i> 
+                </button>
+            </h2>
+        </div>
+
+        <div id="collapseOne{{$f->id}}" class="collapse" aria-labelledby="headingOne{{$f->id}}" data-parent="#accordionExample">
+            <div class="card-body">
+                {{$f->answer}}
+            </div>
+        </div>
+    </div>
+    @endforeach
+</div>
                 </div>
 
              
@@ -39,3 +38,15 @@
         </div>
     </section>
     @endif
+    <script>
+    document.addEventListener("DOMContentLoaded", function () {
+        // استهداف جميع العناصر التي تحتوي على collapse
+        $('.collapse').on('shown.bs.collapse', function () {
+            $(this).prev().find(".icon").removeClass("fa-plus").addClass("fa-minus");
+        });
+
+        $('.collapse').on('hidden.bs.collapse', function () {
+            $(this).prev().find(".icon").removeClass("fa-minus").addClass("fa-plus");
+        });
+    });
+</script>
