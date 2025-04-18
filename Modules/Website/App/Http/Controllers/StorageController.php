c8f383637c701c3257c912e60aefc2ecaa1d70e2
tpct <eslamjackie@outlook.com>
2025-04-18 19:43:45 +0200

    adding webp images, minify

--- Modules/Website/App/Http/Controllers/StorageController.php
+++ Modules/Website/App/Http/Controllers/StorageController.php
@@ -5,6 +5,7 @@ namespace Modules\Website\App\Http\Controllers;
 use App\Helpers\WebpImage;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 
 class StorageController extends Controller
 {
@@ -16,10 +17,15 @@ class StorageController extends Controller
         if (!file_exists($path))
             abort(404);
 
+        if ($cache_path = Cache::get($path))
+            return response()->download($cache_path);
+
         if (@is_array(getimagesize($path))){
             $image_data = WebpImage::convert($path);
             if ($image_data->status) {
                 $path = implode("/", $image_data->fullPath);
+
+                \Cache::put($path, $image_data->fullPath, 60 * 60 * 24 * 30 * 365);
                 return response()->download($path);
             }
             return response()->download($path);
